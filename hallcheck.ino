349270310ad50177d5a69597c58cfce5346768f3
akankshaprasad8 <52112581+akankshaprasad8@users.noreply.github.com>
2020-06-12 19:24:00 +0530

    Add files via upload

---
+++ hallcheck.ino
@@ -0,0 +1,14 @@
+        void setup() {
+
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(3,INPUT);
+
+}
+
+void loop() {
+  int sig=digitalRead(3);
+  Serial.println(sig);
+  delay(100);
+  
+}
